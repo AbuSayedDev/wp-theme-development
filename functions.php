0684b809b5eabc2ea1d1a2404e5712bbb6318dde
AbuSayedDev <abusayed285235@gmail.com>
2023-07-10 17:47:01 +0600

    wp theme development project create

---
+++ functions.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * wtd_setup
+ *Sets up theme defaults and registers support for various WordPress features.
+ */
+
+function wtd_setup_theme(){
+
+   // textdomain
+   load_theme_textdomain('textdomain');
+
+   // nav menus
+   register_nav_menus(array(
+      'main-menu' => __('Main Menu', 'textdomain'),
+      'footer-menu' => __('Footer Menu', 'textdomain')
+   ));
+
+}
+
+add_action( 'after_setup_theme', 'wtd_setup_theme' );
+
+
+
+/**
+ * Proper way to enqueue scripts and styles
+ */
+
+ function wtd_enqueue_scripts(){
+   // style
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+   //  main style
+    wp_enqueue_style( 'main-style', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0.0', 'all' );
+
+   //  responsive css
+    wp_enqueue_style( 'responsive-css', get_template_directory_uri() . '/assets/css/responsive.css', array(), '1.0.0', 'all' );
+ }
+
+ add_action( 'wp_enqueue_scripts', 'wtd_enqueue_scripts' );
+ 
+
+//  Register sidebar
+
+ function wtd_widgets_sidebar(){
+   register_sidebar( array(
+      'name' => __('Main Sidebar', 'textdomain'),
+      'id' => 'sidebar-1',
+      'description' => __('WordPress Theme Development Maib Sidebar', 'textdomain'),
+      'before_widget' => '<div class="about-menu">',
+      'after_widget' => '</div>',
+      'before_title'  => '<h4>',
+		'after_title'   => '</h4>'
+   ));
+ }
+
+ add_action( 'widgets_init', 'wtd_widgets_sidebar' );
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+    <!-- Banner Start Here -->
+    <div class="banner">
+        <img src="<?php echo get_template_directory_uri( ); ?>/assets/img/bannar.jpg" alt="Banner">
+    </div>
+    <!-- Banner End Here -->
+    
+    <!-- Services Start Here -->
+    <?php get_template_part('template-parts/content', 'services'); ?>
+    <!-- Services End Here -->
+    
+    <!-- About Start Here -->
+    <div class="about fix" id="about">
+        <div class="about-left">
+            <h4>about us</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum architecto quaerat praesentium impedit. Aut libero, magnam non totam consectetur velit reprehenderit soluta officiis minima tempora.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum architecto quaerat praesentium impedit. Aut libero, magnam non totam consectetur velit reprehenderit soluta officiis minima tempora.</p>
+            <a href="about.html" class="btn">read more</a>
+        </div>
+        
+        <?php get_sidebar(); ?>
+    </div>
+    <!-- About End Here -->
+
+    <?php get_footer(); ?>
