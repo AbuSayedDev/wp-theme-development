e8840d79a6ed1ef84252512bc6eef0622a509682
AbuSayedDev <abusayed285235@gmail.com>
2023-07-18 18:35:15 +0600

    Home page Blog section added and query set

--- template-home.php
+++ template-home.php
@@ -20,12 +20,66 @@
 <!-- About Start Here -->
 <div class="about fix" id="about">
     <div class="about-left">
-        <h4>about us</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum architecto quaerat praesentium impedit. Aut libero, magnam non totam consectetur velit reprehenderit soluta officiis minima tempora.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum architecto quaerat praesentium impedit. Aut libero, magnam non totam consectetur velit reprehenderit soluta officiis minima tempora.</p>
-        <a href="about.html" class="btn">read more</a>
+        <div class="about-left-content">
+            <h4>about us</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum architecto quaerat praesentium impedit. Aut libero, magnam non totam consectetur velit reprehenderit soluta officiis minima tempora.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum architecto quaerat praesentium impedit. Aut libero, magnam non totam consectetur velit reprehenderit soluta officiis minima tempora.</p>
+            <a href="about.html" class="btn">read more</a>
+        </div>
+
+        <div class="service service-page fix">
+            <div class="about-left-blog">
+
+            <h4>Latest Post</h4>
+
+            <?php
+
+                $args = array(
+                    'post_type'=> 'post',
+                    'posts_per_page' => 2
+
+                );
+
+                $query = new WP_Query($args);
+
+                    if($query->have_posts()){
+                        while($query->have_posts()){
+                            $query->the_post();
+
+
+            ?>
+
+                        <div class="single-service blogs">
+                            <h4><?php the_title();?></h4>
+                            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('', ['class' => 'img-responsive responsive--full', 'title' => 'Feature image']); ?></a>
+                            
+                            <p><?php the_author_posts_link();?> | <?php the_date('F d');?> | <?php the_category(' ', 1);?></p>
+
+                            <p><?php the_excerpt(); ?></p>
+                            <a href="<?php the_permalink(); ?>" class="btn">read more</a>
+                        </div>
+
+                        <?php  }
+                
+
+                        wp_reset_postdata();
+
+                    }else{ ?>
+
+                        <p>No Post</p>
+                    
+                    <?php  } 
+            ?>
+                
+            </div>
+        </div>
     </div>
 
+
+
+
+
+    <!-- sidebar -->
     <?php get_sidebar(); ?>
 </div>
 <!-- About End Here -->
