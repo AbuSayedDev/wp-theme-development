ad10982ac1468f2a4995531f5fa6249bdcbf636d
AbuSayedDev <abusayed285235@gmail.com>
2023-07-30 18:11:19 +0600

    widget form title and description section create

--- functions.php
+++ functions.php
@@ -158,10 +158,31 @@ class My_Widget extends WP_Widget{
 
 
 	public function widget( $args, $instance ){
-		echo 'Hello My First Widget';
+
+		echo '<h4>' .$instance['title']. '</h4>';
+		echo '<p>' .$instance['description']. '</p>';
 	}
 
-	// public function form( ) {}
+	public function form($instance) {
+		$title = $instance['title'];
+		$description   = $instance['description'];
+		?>
+
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>">Title</label>
+			<input type="text" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $title ?>" class="widefat"> 
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id('description'); ?>">Description</label>
+			<textarea name="<?php echo $this->get_field_name('description'); ?>" id="<?php echo $this->get_field_id('description'); ?>" value="<?php echo $description ?>" class="widefat"></textarea> 
+		</p>
+
+
+		<?php
+	}
+
+
 	// public function update(){}
 
 
